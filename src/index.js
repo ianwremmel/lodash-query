8126cc8eeebcbd782b2517068c5f41c637e6bf1e
Ian Remmel <design@ianwremmel.com>
2014-03-06 22:09:07 -0800

    added tokenizer pattern

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@
 // var _ = require('lodash');
 
 module.exports = {
+  pattern: /\.([^\.\[]+)(?:\[(?:([^\.])*?(?:([=<>(?:<=)(?:>=)])\"?(.*?)\"?)?)\])?/g,
+
   query: function(predicate, data) {
 
     // TODO can't use split in case there is a period inside a predicate;
     // instead, need to use regex that requires parens be closed
-    // var  pattern = /(\w+)(?:[(\d+)|(\w+)|(?:(\w)(.{1})(\w))])?/;
-    var pattern = /\.([^\.]*)/;
-    console.log(pattern.exec(predicate));
+    var match;
+    while ((match = this.pattern.exec(predicate)) !== null) {
+      console.log(predicate, match);
+    }
+
+    // console.log(predicate, this.pattern.exec(predicate));
     data = data;
+
     // var result = _.reduce(predicate.split('.'), function(result, predicate) {
     //   console.log(predicate);
     //   if (!predicate) {
